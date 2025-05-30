2e6192540bd4e97a6743049f33e037e9f7cdc5c8
GaelIb <ibayezgael@gmail.com>
2025-05-29 18:34:03 -0600

    Update Service.cpp

--- Service.cpp
+++ Service.cpp
@@ -1,19 +1,22 @@
 #include "Service.h"
 
-
+// Constructores
 Service::Service() : serviceName(""), duration(0), basePrice(0.0) {}
 
 Service::Service(const string& name, int duration, double basePrice)
     : serviceName(name), duration(duration), basePrice(basePrice) {}
 
+// Getters
 string Service::getService() const { return serviceName; }
 int Service::getDuration() const { return duration; }
 double Service::getBasePrice() const { return basePrice; }
 
+// Setters
 void Service::setServiceName(const string& name) { serviceName = name; }
 void Service::setDuration(int duration) { this->duration = duration; }
 void Service::setBasePrice(double price) { basePrice = price; }
 
+// Overload
 double Service::calculateFinalPrice(bool isReturningClient) const {
     return basePrice;
 }
